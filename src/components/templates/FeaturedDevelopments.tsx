8da8d9697f5785e481490c52bf55fd91a2254146
Taras.Kravets <taraskravets95@gmail.com>
2023-05-05 13:36:03 +0300

    feat: added assets to Featured developments

---
+++ src/components/templates/FeaturedDevelopments.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const FeaturedDevelopmentsStyled = styled.section``;
+
+export default function FeaturedDevelopments() {
+	return (
+		<FeaturedDevelopmentsStyled>
+			FeaturedDevelopments
+		</FeaturedDevelopmentsStyled>
+	);
+}
